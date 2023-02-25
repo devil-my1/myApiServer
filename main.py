ccae16db01f611880a279ee8e5f12825ba9fc9b5
Fallen <amir.amon@mail.ru>
2023-02-26 00:19:52 +0900

    add post method

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status,Request
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -6,6 +7,17 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/items/{item_id}/description")
+async def read_item_description(item_id: int):
+    item_description = "This is a sample item."
+    headers = {"X-Custom-Header": "some-value"}
+    return JSONResponse(content={"description": item_description}, status_code=status.HTTP_200_OK, headers=headers)
+
+
+@app.post("/items/")
+async def create_item(request: Request):
+    data = await request.json()
+    # Do some processing with the received data
+    item_id = 123  # This could be a generated ID or any other value
+    item = {"id": item_id, "name": data["name"], "description": data["description"]}
+    return JSONResponse(content=item)
