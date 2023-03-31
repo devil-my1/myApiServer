93c9fb5450467d5e6296c128ec51fd7ed27ba086
Fallen <amir.amon@mail.ru>
2023-04-01 01:34:39 +0900

    fix logger and add download story func

--- Controler/inst_api_manager.py
+++ Controler/inst_api_manager.py
@@ -1,9 +1,10 @@
+from pathlib import Path
 import instagrapi
 from instagrapi import Client
 from instagrapi.types import StoryMention, StoryMedia, StoryLink, StoryHashtag
 import os
 
-from common import logger
+from common.logger import logger
 
 
 cl = Client()
@@ -12,7 +13,7 @@ cl.login(os.getenv("INST_USERNAME"), os.getenv("INST_PASSWORD"))
 
 
 class User:
-    def __init__(self, user_name: str) -> None:
+    def __init__(self, user_name: str = "") -> None:
         """Constructor for use basic method to access Instagram API
 
         Args:
@@ -49,4 +50,37 @@ class User:
             else:
                 return {"Info": "First check user info to get user stories!"}
         except Exception as err:
-            return {"Error": err}
+            return {"Error": str(err)}
+
+    @classmethod
+    def download_story(
+        self, url: str = None, story_id: str = None, file_name: str = "{}_story"
+    ):
+        """Download user stories by url or id
+
+        Args:
+            url (str, optional): Story's urs. Defaults to None.
+            story_id (str, optional): Story's id. Defaults to None.
+            file_name (str, optional): Save downloaded file name. Defaults to "{}_story.mp4".
+        """
+        folder = Path("./inst_data/stories/")
+        if not folder.exists():
+            folder.mkdir(parents=True)
+
+        file_name = file_name.format(
+            len([file for file in folder.glob("*") if Path.is_file(file)]) + 1
+        )
+        res = None
+        logger.info(
+            "Story's filename: [%s] id: [%s] url: [%s]", file_name, story_id, url
+        )
+
+        try:
+            if story_id:
+                res = cl.story_download(story_id, file_name, folder)
+            elif url:
+                res = cl.story_download_by_url(url, file_name, folder)
+
+            return {"File downloaded path": str(res)}
+        except Exception as err:
+            return {"Error": str(err)}
